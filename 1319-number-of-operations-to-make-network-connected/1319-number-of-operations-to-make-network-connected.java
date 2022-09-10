17e8d260234805d248a8959a67267f727d5d6bcb
ashish94170 <96986601+ashish94170@users.noreply.github.com>
2022-09-10 23:05:43 +0530

    Time: 33 ms (33.54%), Space: 77.7 MB (21.43%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -0,0 +1,37 @@
+//Similar to number of islands
+class Solution {
+
+    private void dfs(int x, ArrayList<ArrayList<Integer>> adj, boolean[] vis, int n) {
+        vis[x] = true;
+        for(Integer i: adj.get(x))
+        {
+            if(!vis[i])
+                dfs(i,adj,vis,n);
+        }
+    }
+
+    public int makeConnected(int n, int[][] connections) {
+        int c = 0;
+        if (n > (connections.length + 1)) return -1;
+        
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        
+        for(int i=0;i<n;i++)
+            adj.add(new ArrayList<Integer>());
+        
+        for(int i=0;i<connections.length;i++)
+        {
+            adj.get(connections[i][0]).add(connections[i][1]);
+            adj.get(connections[i][1]).add(connections[i][0]);
+        }
+        
+        boolean[] vis = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                c++;
+                dfs(i, adj, vis,n);
+            }
+        }
+        return c - 1;
+    }
+}
