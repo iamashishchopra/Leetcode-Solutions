14915d6f0dd1b1e64f91eb960a99d4f32196e2db
ashish94170 <96986601+ashish94170@users.noreply.github.com>
2022-10-23 01:01:42 +0530

    Time: 0 ms (100.00%), Space: 42 MB (37.59%) - LeetHub

--- 1145-binary-tree-coloring-game/1145-binary-tree-coloring-game.java
+++ 1145-binary-tree-coloring-game/1145-binary-tree-coloring-game.java
@@ -16,38 +16,25 @@
 class Solution {
 
     public boolean btreeGameWinningMove(TreeNode root, int n, int x) {
-        TreeNode cur = dfs(root, x);
-        System.out.println(cur.val);
-        int left = 0;
-        int right = 0;
-        if (cur.left != null) left = countNodes(cur.left);
-        if (cur.right != null) right = countNodes(cur.right);
+        int val[]=new int[2];
+        countNodes(root,val,x);
+        int left=val[0];
+        int right=val[1];
         int parent = n - left - right - 1;
-        //System.out.println(parent);
         if (left > n / 2 || right > n / 2 || parent > n / 2) return true;
         return false;
     }
-
-    private TreeNode dfs(TreeNode root, int n) {
-        Queue<TreeNode> q=new LinkedList<TreeNode>();
-        TreeNode z=null;
-        q.offer(root);
-        while(!q.isEmpty()){
-            TreeNode temp=q.poll();
-            if(temp.val==n)
-                z=temp;
-            if(temp.left!=null)
-                q.add(temp.left);
-            if(temp.right!=null)
-                q.add(temp.right);
-        }
-        return z;
-    }
-
-    private int countNodes(TreeNode root) {
+    private int countNodes(TreeNode root,int[] val,int x) {
         if (root == null) return 0;
-        int left = countNodes(root.left);
-        int right = countNodes(root.right);
+        
+        int left = countNodes(root.left,val,x);
+        int right = countNodes(root.right,val,x);
+        if(root.val==x){
+            val[0]=left;
+            val[1]=right;
+        }
+            
+            
         return 1 + left + right;
     }
 }
